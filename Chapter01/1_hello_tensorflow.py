966ff5e37c00354b7b65a5f7329195ab361a0040
rajacheers <rajalingappaa@hotmail.com>
2018-01-23 17:43:24 +0800

    deleting older version

--- Chapter01/1_hello_tensorflow.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-session = tf.Session()
-print(session.run(hello))
--- Chapter01/3_add_tensorboard.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-x = tf.placeholder(tf.float32, name='x')
-y = tf.placeholder(tf.float32, name='y')
-z = tf.add(x, y, name='sum')
-session = tf.Session()
-summary_writer = tf.summary.FileWriter('/Users/i335713/Downloads/logs', session.graph)
-summary_writer.flush()
-
